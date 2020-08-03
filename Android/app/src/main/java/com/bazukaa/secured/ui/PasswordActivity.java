b05e4cdf6d1804e78ec0490e232a8e6dfb38d850
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-08-03 12:50:59 +0530

    Basic setup

---
+++ Android/app/src/main/java/com/bazukaa/secured/ui/PasswordActivity.java
@@ -0,0 +1,16 @@
+package com.bazukaa.secured.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bazukaa.secured.R;
+
+public class PasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password);
+    }
+}

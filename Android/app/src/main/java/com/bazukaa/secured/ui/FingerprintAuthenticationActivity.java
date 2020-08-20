31f52a79fedc8030d0aad8a4532d59836a6aa9c4
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-08-20 23:57:35 +0530

    Fingerprint auth added with all possible cases, Not yet applied

--- Android/app/src/main/java/com/bazukaa/secured/ui/FingerprintAuthenticationActivity.java
+++ Android/app/src/main/java/com/bazukaa/secured/ui/FingerprintAuthenticationActivity.java
@@ -4,15 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.KeyguardManager;
 import android.content.pm.PackageManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.security.keystore.KeyProperties;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bazukaa.secured.R;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,6 +47,10 @@ public class FingerprintAuthenticationActivity extends AppCompatActivity {
 
     private FingerprintManager fingerprintManager;
     private KeyguardManager keyguardManager;
+    private KeyStore keyStore;
+    private Cipher cipher;
+    private static final String KEY_NAME = "ANDROID KEY";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +75,66 @@ public class FingerprintAuthenticationActivity extends AppCompatActivity {
                 tvLabel.setText("You should add at least one Fingerprint to use this feature");
             }else{
                 tvLabel.setText("Place your Finger on Scanner to Access the App");
+                generateKey();
+                if(cipherInit()){
+                    FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
+                    FingerprintHandler fingerprintHandler = new FingerprintHandler(this);
+                    fingerprintHandler.startAuth(fingerprintManager, cryptoObject);
+                }
             }
         }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void generateKey() {
+
+        try {
+
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+
+            keyStore.load(null);
+            keyGenerator.init(new
+                    KeyGenParameterSpec.Builder(KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(
+                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+            keyGenerator.generateKey();
+        } catch (KeyStoreException | IOException | CertificateException | NoSuchAlgorithmException | InvalidAlgorithmParameterException | NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public boolean cipherInit() {
+        try {
+            cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/" + KeyProperties.BLOCK_MODE_CBC + "/" + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException("Failed to get Cipher", e);
+        }
+
+
+        try {
+
+            keyStore.load(null);
+
+            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME,
+                    null);
+
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+
+            return true;
+
+        } catch (KeyPermanentlyInvalidatedException e) {
+            return false;
+        } catch (KeyStoreException | CertificateException | UnrecoverableKeyException | IOException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Failed to init Cipher", e);
+        }
+
     }
 }
---
+++ Android/app/src/main/java/com/bazukaa/secured/ui/FingerprintHandler.java
@@ -0,0 +1,58 @@
+package com.bazukaa.secured.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.CancellationSignal;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.core.content.ContextCompat;
+import com.bazukaa.secured.R;
+
+public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
+    private Context context;
+
+    public FingerprintHandler(Context context) {
+        this.context = context;
+    }
+
+    public void startAuth(FingerprintManager fingerprintManager, FingerprintManager.CryptoObject cryptoObject){
+        CancellationSignal cancellationSignal = new CancellationSignal();
+        fingerprintManager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
+    }
+
+    @Override
+    public void onAuthenticationError(int errorCode, CharSequence errString) {
+        this.update("There was an Auth Error" + errString, false);
+    }
+
+    @Override
+    public void onAuthenticationFailed() {
+        this.update("Auth Failed", false);
+    }
+
+    @Override
+    public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+        this.update("Error" + helpString, false);
+    }
+
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+        this.update("You can now access the app", true);
+    }
+
+    private void update(String s, boolean b){
+        TextView tvLabel = ((Activity)context).findViewById(R.id.act_finger_print_auth_tv_label);
+        ImageView fingerprintImg = ((Activity)context).findViewById(R.id.act_finger_print_auth_img_finger);
+
+        tvLabel.setText(s);
+
+        if(b == false){
+            tvLabel.setTextColor(ContextCompat.getColor(context, R.color.colorError));
+        }else{
+            tvLabel.setTextColor(ContextCompat.getColor(context, R.color.colorAccepted));
+            fingerprintImg.setImageResource(R.mipmap.done);
+        }
+
+    }
+}

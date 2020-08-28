942763c48a875c15e4fa1dc1e8dd38edd308b3d1
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-08-29 03:15:31 +0530

    Onboarding setup

---
+++ Android/app/src/main/java/com/bazukaa/secured/ui/OnboardingActivity.java
@@ -0,0 +1,16 @@
+package com.bazukaa.secured.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bazukaa.secured.R;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
